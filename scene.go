452270cd7bf0dbb2871155a01a3e981081b00c3e
Fernando Rodriguez <fernando@nandotech.com>
2018-03-30 12:16:14 -0400

    created scene and drew static bird

---
+++ scene.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/veandco/go-sdl2/img"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+type scene struct {
+	bg   *sdl.Texture
+	bird *sdl.Texture
+}
+
+func newScene(r *sdl.Renderer) (*scene, error) {
+	bg, err := img.LoadTexture(r, "res/imgs/background.png")
+	if err != nil {
+		return nil, fmt.Errorf("could not load background image: %v", err)
+	}
+	bird, err := img.LoadTexture(r, "res/imgs/bird_frame_1.png")
+	if err != nil {
+		return nil, fmt.Errorf("could not load background image: %v", err)
+	}
+	return &scene{bg: bg, bird: bird}, nil
+}
+
+func (s *scene) paint(r *sdl.Renderer) error {
+	r.Clear()
+
+	if err := r.Copy(s.bg, nil, nil); err != nil {
+		return fmt.Errorf("could not copy background: %v", err)
+	}
+
+	rect := &sdl.Rect{X: 10, Y: 300 - 43/2, W: 50, H: 43}
+	if err := r.Copy(s.bird, nil, rect); err != nil {
+		return fmt.Errorf("could not copy background: %v", err)
+	}
+
+	r.Present()
+	return nil
+}
+
+func (s *scene) destroy() {
+	s.bg.Destroy()
+}
